c412f248d8c8b5b388a40fcf7ded748c3e6a1c35
Shifra Wagshall <shifrawagshall@gmail.com>
2022-02-23 22:29:19 -0500

    Created switch statement for all choices.

--- index.js
+++ index.js
@@ -28,8 +28,61 @@ const db = mysql.createConnection(
             name: "index",
             type: "list",
             message: "What would you like to do?",
-            choices: ["View all Employees","Add Employee",  "Remove an Employee", "Update Employee Role",  
+            choices: ["View All Employees","Add Employee",  "Remove an Employee", "Update Employee Role",  
             "View All Roles", "Add a Role", "View all Departments", "Add a Department",  "Remove a Department", 
             "View all Employees",  "Quit"]
         })
+  .then(function(choice) {
+    switch (choice) {
+        case "View All Employees":
+          viewAllEmployees();
+        break;
+
+        case "Add Employee":
+          addEmps();
+        break;
+        
+        case "Remove Employee":
+          removeEmps();
+        break;
+
+        case "Update Employee Role":
+          updateEmpRole();
+        break;
+        
+        case "View All Roles":
+          viewAllRoles();
+        break;
+
+        case "Add Role":
+            addRole();
+        break;
+
+        case "View All Department":
+            viewAllDeps();
+        break;
+
+        case "Add Department":
+            addDep();
+        break;
+
+        case "Remove Department":
+          removeDep();
+        break;
+
+        case "Add Employee":
+            addEmps();
+        break;
+        
+        case "Quit":
+            console.log("Program Ended");
+            connection.end();
+        break;
+
+        }
+});
   }
+
+
+  //functions for each switch case 
+  

f5cfebdfa063f0f12b6bdf3c61db32bd2e0539cd
Shifra Wagshall <shifrawagshall@gmail.com>
2022-02-24 01:17:04 -0500

    Added a role and an emp.

--- empSeed.sql
+++ empSeed.sql
@@ -10,4 +10,15 @@ VALUE ("Finance");
 INSERT INTO departments (name)
 VALUE ("Legal");
 
+
+-- roles
+INSERT INTO roles (title, salary, dep_id)
+VALUES ("Accountant", 50000, 3);
+
+-- emps 
+INSERT INTO emps (firstName, lastName, role_id)
+VALUES ("Miles", "Kay", 1);
+
+
+
 SELECT * FROM departments;

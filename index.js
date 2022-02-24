c21e9fe2dacfdaf1bee4cb36d33162fd52decaf2
Shifra Wagshall <shifrawagshall@gmail.com>
2022-02-23 21:26:18 -0500

    imported all the req packages, and created a connection to the db.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const mysql = require('mysql2');
+const inquirer = require("inquirer");
+const consTable = require('console.table');
 
+// console.log("hiiii");
+
+//cr8 the connection to the sql db
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // MySQL password
+      password: 'Chemdas.1',
+      database: 'emps_db'
+    },
+    console.log(`Connected to the emps_db database.`)
+  );

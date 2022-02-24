0f5e9084fb63294f8a6b4841b80b821ea9d14948
Shifra Wagshall <shifrawagshall@gmail.com>
2022-02-24 00:37:33 -0500

    Added deps data to the seed.sql.

--- empSeed.sql
+++ empSeed.sql
@@ -1,1 +1,13 @@
+USE emps_db;
 
+-- Adding the deps
+INSERT INTO departments (name)
+VALUE ("Sales");
+INSERT INTO departments (name)
+VALUE ("Engineering");
+INSERT INTO departments (name)
+VALUE ("Finance");
+INSERT INTO departments (name)
+VALUE ("Legal");
+
+SELECT * FROM departments;

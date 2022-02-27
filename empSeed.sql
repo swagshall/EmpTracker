e63428510ad1d14ed089b79e64ffeca1c64ce28a
Shifra Wagshall <shifrawagshall@gmail.com>
2022-02-27 14:55:41 -0500

    Added nore data to the seeds file.

--- empSeed.sql
+++ empSeed.sql
@@ -14,11 +14,24 @@ VALUE ("Legal");
 -- roles
 INSERT INTO roles (title, salary, dep_id)
 VALUES ("Accountant", 50000, 3);
+INSERT INTO roles (title, salary, dep_id)
+VALUES ("Back-end Developer", 1000000, 2);
+INSERT INTO roles (title, salary, dep_id)
+VALUES ("Sales-men", 45000, 1);
+INSERT INTO roles (title, salary, dep_id)
+VALUES ("Lawyer", 150000, 4);
 
 -- emps 
 INSERT INTO emps (firstName, lastName, role_id)
 VALUES ("Miles", "Kay", 1);
-
-
+INSERT INTO emps (firstName, lastName, role_id)
+VALUES ("Sam", "Martin", 2);
+INSERT INTO emps (firstName, lastName, role_id)
+VALUES ("Larry", "Smith", 3);
+INSERT INTO emps (firstName, lastName, role_id)
+VALUES ("John", "Brown", 4);
 
 SELECT * FROM departments;
+SELECT * FROM roles;
+SELECT * FROM emps;
+
